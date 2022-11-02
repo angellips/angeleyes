9a8828f781cbf32a74cd705e6de199b3195c2f49
angellips <85476798+angellips@users.noreply.github.com>
2022-11-02 07:14:41 -0400

    added comments and small edits

--- QuickScripts/cc-normalize.py
+++ QuickScripts/cc-normalize.py
@@ -13,7 +13,7 @@ all_seeds = []
 most_common = []
 
 for x in files:
-    find = re.search('.*\d[-]\d+[-]\w+', x)
+    find = re.search('.*\d[-]\d+[-]\w+', x) # finds match in format of "00001-2259320428-green tree; bright sun; blue water.png" 
     if find != None:
         mods = find.string.replace('.png', '')
         mods = mods.replace(';', '')
@@ -28,9 +28,9 @@ for x in files:
 
 all_modifiers = (list(chain.from_iterable(all_modifiers))) # flatten nested list
 
-for m in range(10):
-    most = (Counter(all_modifiers).most_common(10)[m][0])
-    most_common.append(most) # creates top 10 list of most used modifiers
+for m in range(5):
+    most = (Counter(all_modifiers).most_common(5)[m][0])
+    most_common.append(most) # creates top 5 list of most used modifiers
 
 dp = "{"
 
@@ -46,7 +46,7 @@ for x in itertools.permutations(most_common):
 while not(count % 8) == False:
     count += 1
 
-batch_size = int(count / 8)
+batch_size = int(count / 8) # batch size * batch count in Dynamic Prompting sets the *max* number of images to produce; count of img would remain same and is produced via combinatorial option.
 
 print(f"\nThe following would generate {count} different variations in Dynamic Prompting:")
 print("\n" + dp)
