8b12943e7be36647d3a64af7e37c46e051092528
angellips <85476798+angellips@users.noreply.github.com>
2022-11-02 11:16:24 -0400

    added single and chunk modifier to txt file

--- QuickScripts/cc-normalize.py
+++ QuickScripts/cc-normalize.py
@@ -1,5 +1,7 @@
 # simple skeleton structure of script to analyze prompt texts and create normalized data for use in cc-study
 
+# c_prompts and s_prompts txt files must exist before running (fix later)
+
 import os
 import re
 import itertools
@@ -8,52 +10,83 @@ from collections import Counter
 
 files = os.listdir('sample_images')
 
-all_modifiers = []
 all_seeds = []
-most_common = []
+single_modifiers = []
+single_most = []
+chunk_modifiers = []
+chunk_most = []
+
 
 for x in files:
-    find = re.search('.*\d[-]\d+[-]\w+', x) # finds match in format of "00001-2259320428-green tree; bright sun; blue water.png" 
+    find = re.search('.*\d[-]\d+[-]\w+', x)                 # finds match in format of "00001-2259320428-green", inherently excludes all other files.
     if find != None:
         mods = find.string.replace('.png', '')
-        mods = mods.replace(';', '')
+        c_mods = mods.split('-')
+        c_mods = c_mods[2:]
+        chunk_modifiers.append(c_mods[0].split(";"))        # creates chunk-modifiers, e.g., "hello world; blue sky; cool dog", returns each ; separated chunk of text
+        s_mods = mods.replace(';', '')
+        s_mods = s_mods.split('-')
+        s_mods.pop(0)
+        s_mods.pop(0)
+        s_mods = s_mods[0].split()
+        single_modifiers.insert(0, s_mods)                  # creates single modifiers, e.g., "hello", "world", "blue", etc..
         mods = mods.split('-')
         seed_clean = mods[:-1]
-        seed_clean.pop(0)
-        all_seeds.append(seed_clean)
-        mods.pop(0)
-        mods.pop(0)
-        mods = mods[0].split()
-        all_modifiers.insert(0, mods) # nested list
+        seed_clean = seed_clean[1:]
+        all_seeds.append(seed_clean)                                  # creates nested list of all seeds
 
-all_modifiers = (list(chain.from_iterable(all_modifiers))) # flatten nested list
+chunk_modifiers = (list(chain.from_iterable(chunk_modifiers)))        # flatten nested list
+single_modifiers = (list(chain.from_iterable(single_modifiers)))      # flatten nested list
 
 for m in range(5):
-    most = (Counter(all_modifiers).most_common(5)[m][0])
-    most_common.append(most) # creates top 5 list of most used modifiers
+    s_most = (Counter(single_modifiers).most_common(5)[m][0])
+    single_most.append(s_most)                                        # creates top 5 list of most used single modifiers
+    c_most = (Counter(chunk_modifiers).most_common(5)[m][0])
+    chunk_most.append(c_most)                                         # creates top 5 list of most used single modifiers
 
 dp = ""
 
-for x in most_common:
+for x in single_most:
     dp = dp + f"{x}" + " "
 
 count = 0
 prompts = []
-for x in itertools.permutations(most_common):
+for x in itertools.permutations(single_most):
     count += 1
     prompts.append(x)
 
-del_lines = open('prompts.txt', 'r+')  # clear all lines from prompts.txt (if any) and then open for appending prompt variations
+del_lines = open('s_prompts.txt', 'r+')                 # clear all lines from s_prompts.txt (if any) and then open for appending prompt variations
 del_lines.truncate(0)
 del_lines.close()
-p_txt = open("prompts.txt","a")
+p_txt = open("s_prompts.txt","a")
 
-for x in (map(str, prompts)): # alters truple elements into strings and then cleans them up for writing to txt
+for x in (map(str, prompts)):                           # alters truple elements into strings and then cleans them up for writing to txt
     x = x[1:]
     x = x[:-1]
     x = x.replace("'", "")
     x = x.replace(",", "")
     p_txt.write(x + "\n")
+
+p_txt.close()
+
+count = 0
+prompts = []
+for x in itertools.permutations(chunk_most):
+    count += 1
+    prompts.append(x)                                   
+
+del_lines = open('c_prompts.txt', 'r+')                 # clear all lines from c_prompts.txt (if any) and then open for appending prompt variations
+del_lines.truncate(0)
+del_lines.close()
+p_txt = open("c_prompts.txt","a")
+
+for x in (map(str, prompts)):                           # alters truple elements into strings and then cleans them up for writing to txt
+    x = x[1:]
+    x = x[:-1]
+    x = x.replace("'", "")
+    x = x.replace(",", ";")
+    x = x.replace(" ", " ")
+    p_txt.write(x + "\n")                               # spits out lines wonky but can be ctrl-h fixed; need to fix in here
 
 p_txt.close()
 
