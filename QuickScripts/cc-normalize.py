6d9a5b5793928909b6f61e820d8163504c56af58
angellips <85476798+angellips@users.noreply.github.com>
2022-11-05 15:37:38 -0400

    added file_search function

--- QuickScripts/cc-normalize.py
+++ QuickScripts/cc-normalize.py
@@ -79,8 +79,6 @@ def seq_match(mod_most):
     #print("BREAK")
     #print(b_values)
 
-# add sort and filtering (include this earlier in code during the single/chunk_most analysis)
-
 def file_work(mod_most, mod_type):
 
     print(mod_most, mod_type)
@@ -99,9 +97,9 @@ def file_work(mod_most, mod_type):
         prompts.append(x)
 
     if mod_type == "single_most":
-        del_lines = open('s_prompts.txt', 'r+')                 # clear all lines from s_prompts.txt (if any) and then open for appending prompt variations
+        del_lines = open('s_prompts.txt', 'r+')                                     # clear all lines from s_prompts.txt (if any) and then open for appending prompt variations
     elif mod_type == "chunk_most":
-        del_lines = open('c_prompts.txt', 'r+')                 # clear all lines from s_prompts.txt (if any) and then open for appending prompt variations
+        del_lines = open('c_prompts.txt', 'r+')                                     # clear all lines from s_prompts.txt (if any) and then open for appending prompt variations
 
     del_lines.truncate(0)
     del_lines.close()
@@ -111,7 +109,7 @@ def file_work(mod_most, mod_type):
     elif mod_type == "chunk_most":
         p_txt = open("c_prompts.txt","a")
 
-    for x in (map(str, prompts)):                           # alters truple elements into strings and then cleans them up for writing to txt
+    for x in (map(str, prompts)):                                                   # alters truple elements into strings and then cleans them up for writing to txt
         x = x[1:]
         x = x[:-1]
         x = x.replace("'", "")
@@ -123,47 +121,49 @@ def file_work(mod_most, mod_type):
 
     p_txt.close()
 
-for x in files:
-    find = re.search('.*\d[-]\d+[-]\w+', x)                 # finds match in format of "00001-2259320428-green", inherently excludes all other files.
-    if find != None:
-        find = re.sub('(?<![a-z])-(?![a-z])', '_', x)
-        find = re.sub('(?<![a-z])-(?!\d)', '_', x)
-        mods = find.replace('.png', '')
-        c_mods = mods.split('_')
-        c_mods = c_mods[1:]
-        chunk_modifiers.append(c_mods[0].split(";"))        # creates chunk-modifiers, e.g., "hello world; blue sky; cool dog", returns each ; separated chunk of text
-        s_mods = mods.replace(';', '')
-        s_mods = s_mods.split('_')
-        s_mods = s_mods[1:]
-        single_modifiers.append(s_mods[0].split())                  # creates single modifiers, e.g., "hello", "world", "blue", etc..
-        mods = mods.split('-')
-        seed_clean = mods[1:]
-        seed_clean = seed_clean[0].split('_')
-        seed_clean = seed_clean[:-1]
-        all_seeds.append(seed_clean)  # creates nested list of all seeds
+def file_search(files, single_modifiers, chunk_modifiers):
+    for x in files:
+        find = re.search('.*\d[-]\d+[-]\w+', x)                                         # finds match in format of "00001-2259320428-green", inherently excludes all other files.
+        if find != None:
+            find = re.sub('(?<![a-z])-(?![a-z])', '_', x)
+            find = re.sub('(?<![a-z])-(?!\d)', '_', x)
+            mods = find.replace('.png', '')
+            c_mods = mods.split('_')
+            c_mods = c_mods[1:]
+            chunk_modifiers.append(c_mods[0].split(";"))                                # creates chunk-modifiers, e.g., "hello world; blue sky; cool dog", returns each ; separated chunk of text
+            s_mods = mods.replace(';', '')
+            s_mods = s_mods.split('_')
+            s_mods = s_mods[1:]
+            single_modifiers.append(s_mods[0].split())                                  # creates single modifiers, e.g., "hello", "world", "blue", etc..
+            mods = mods.split('-')
+            seed_clean = mods[1:]
+            seed_clean = seed_clean[0].split('_')
+            seed_clean = seed_clean[:-1]
+            all_seeds.append(seed_clean)                                                # creates nested list of all seeds
 
-chunk_modifiers = (list(chain.from_iterable(chunk_modifiers)))        # flatten nested list
-single_modifiers = (list(chain.from_iterable(single_modifiers)))      # flatten nested list
+    chunk_modifiers = (list(chain.from_iterable(chunk_modifiers)))                      # flatten nested list
+    single_modifiers = (list(chain.from_iterable(single_modifiers)))                    # flatten nested list
 
-for m in range(5):
-    s_most = (Counter(single_modifiers).most_common(5)[m][0])
-    single_most.append(s_most)                                        # creates top 5 list of most used single modifiers
-    
-    if re.search('^\w', (Counter(chunk_modifiers).most_common(5)[m][0])) != None:   # fixes lack of space after c_mods = mods.split('-')
-        c_most = (Counter(chunk_modifiers).most_common(5)[m][0])
-        c_most = " " + c_most
-        chunk_most.append(c_most)
-    else:
-        c_most = (Counter(chunk_modifiers).most_common(5)[m][0])
-        chunk_most.append(c_most)
+    for m in range(5):
+        s_most = (Counter(single_modifiers).most_common(5)[m][0])
+        single_most.append(s_most)                                                      # creates top 5 list of most used single modifiers
+        
+        if re.search('^\w', (Counter(chunk_modifiers).most_common(5)[m][0])) != None:   # fixes lack of space after c_mods = mods.split('-')
+            c_most = (Counter(chunk_modifiers).most_common(5)[m][0])
+            c_most = " " + c_most
+            chunk_most.append(c_most)
+        else:
+            c_most = (Counter(chunk_modifiers).most_common(5)[m][0])
+            chunk_most.append(c_most)
 
+file_search(files, single_modifiers, chunk_modifiers)
 file_work(single_most, "single_most")
 file_work(chunk_most, "chunk_most")
 
-while not(count % 8) == False:                          # rounds up prompt count until batch_size will actually allow all images to be generated
+while not(count % 8) == False:                                                      # rounds up prompt count until batch_size will actually allow all images to be generated; # of img generated stays the same
     count += 1
 
-batch_size = int(count / 8) # batch num * batch count in Dynamic Prompting sets the *max* number of images to produce; count of img would remain same and is produced via combinatorial option.
+batch_size = int(count / 8)
 
 print(f"\nThe following will generate {count} prompt permutations in Dynamic Prompting:")
 print("\n" + dp)
